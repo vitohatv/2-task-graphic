f99f1c88da57151e14de3ea5cc276df4c4d3f36e
Vitaliy <vitohatv@gmail.com>
2024-02-29 01:40:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import BarChart from './Container/graphic/graphic';
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Container/graphic/graphic.jsx
@@ -0,0 +1,4 @@
+import { ResponsiveBar } from '@nivo/bar';
+import { useState } from 'react';
+import './graphic.css';
+
